d86b5e59959421b9cc671e5dc0412dcb55a9b738
Sehyeon Hong <119861971+c1oud99@users.noreply.github.com>
2023-02-16 02:50:40 +0900

    Add files via upload

---
+++ ch02/01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("이름: 홍길동\n나이: 17\n주소: 안드로메다 시티");
+	return 0;
+}
---
+++ ch02/basic.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("»×ÀïÀÌ~\n");
+	printf("»×»×ÀÌ~");
+
+	return 0;
+}
